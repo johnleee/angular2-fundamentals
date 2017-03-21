bffef275a3e868608eb4300590a337d1d915a0d4
John Lee <jlee@openlane.com>
2017-03-21 10:10:14 -0700

    share services and data with angular2 dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service'
 
 @Component({
   selector: 'app-root',
   template: `<div>
 
 <app-simple-form></app-simple-form>
+
+{{mail.message}}
+
   </div>
   `
 })
 export class AppComponent {
   title = `Let's get started!`;
+
+  constructor(private mail:MailService){}
 }
