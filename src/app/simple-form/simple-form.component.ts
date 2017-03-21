5e37b33f0eac0f6f421f8f001101465cbbe8a837
John Lee <jlee@openlane.com>
2017-03-21 12:46:10 -0700

    apply CSS classes conditionally with ngClass

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-<input #myInput type="text" [(ngModel)]="message">
-<button (mouseover)="update.emit({text:message})">Click me!</button>
+<input 
+  #myInput 
+  type="text" 
+  [(ngModel)]="message"
+  [ngClass]="{mousedown:isMousedown}"
+  (mousedown)="isMousedown = true"
+  (mouseup)="isMousedown = false"
+  (mouseleave)="isMousedown = false"
+  >
+<button (click)="update.emit({text:message})">Click me!</button>
   `,
   styles: [`
 :host{
   display: flex;
   flex-direction:column;
+}
+
+.mousedown{
+  border: 2px solid green;
 }
 
 *{
@@ -28,6 +40,8 @@ button{
 `]
 })
 export class SimpleFormComponent implements OnInit {
+
+  isMousedown;
 
   @Input() message;
 
